0c84cde7f118e478fc229eb6b6b74d3057d81c69
dani <dan_yeet@protonmail.com>
2025-01-18 19:30:03 +0200

    fixed exercise

--- 1.2.1.c
+++ 1.2.1.c
@@ -5,15 +5,14 @@ main()
 {
     float fahr, celsius;
     float lower, upper, step;
-    lower = 0;   /* lower limit of temperatuire scale */
-    upper = 300; /* upper limit */
-    step = 20;   /* step size */
-    fahr = lower;
-    printf("Celsius Fahrenheit\n");
-    while (fahr <= upper)
+    lower = -20; /* lower limit */
+    upper = 50;  /* upper limit */
+    step = 5;    /* step size */
+    celsius = lower;
+    while (celsius <= upper)
     {
-        celsius = (5.0 / 9.0) * (fahr - 32.0);
-        printf("%3.0f %6.1f\n", celsius, fahr);
-        fahr = fahr + step;
+        fahr = ((celsius * 9)/5) +32  ; /* обратната формула */
+        printf("%6.1f %3.0f\n", celsius, fahr);
+        celsius = celsius + step;
     }
 }
