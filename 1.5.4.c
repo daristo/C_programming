8e668b930f2a5b52fb760b6b6474dffc5341a2f0
daristo <dan_yeet@protonmail.com>
2025-03-29 17:39:10 +0200

    added 1-12 - WIP

---
+++ 1.5.4.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#define IN 1  /* inside a word */
+#define OUT 0 /* outside a word */
+int main(void)
+{
+
+    int c, prev, previous;
+    // state = OUT;
+    previous = 0;
+    prev = (previous != ' ' || previous != '\n' || previous != '\t');
+    // printf("%d",prev);
+    while ((c = getchar()) != EOF)
+    {
+
+        if (c == ' ' || c == '\n' || c == '\t')
+        {
+            //printf("\n");
+            printf("Empty! ");
+            
+        }
+        else
+        {
+            printf("Not empty! ");
+            //putchar(c);
+        }
+        previous = c;
+    }
+}
